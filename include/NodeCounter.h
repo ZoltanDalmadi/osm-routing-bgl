900b2cd974073a0669b4fe3d0aad0d99d9a8b56f
Zoltan Dalmadi <dmz985@gmail.com>
2014-12-07 19:05:09 +0100

    Separated NodeCounter struct

---
+++ include/NodeCounter.h
@@ -0,0 +1,13 @@
+#include <set>
+#include <osmium/visitor.hpp>
+#include <osmium/handler.hpp>
+
+/*
+ * Collects unique way nodes in an osm file.
+ * nodeSet.size() will give you the number of nodes.
+ */
+struct NodeCounter : public osmium::handler::Handler
+{
+  std::set<osmium::object_id_type> nodeSet;
+  void way(const osmium::Way&);
+};
---
+++ src/NodeCounter.cpp
@@ -0,0 +1,12 @@
+#include "NodeCounter.h"
+
+void NodeCounter::way(const osmium::Way& way)
+{
+  const char *highway = way.tags()["highway"];
+
+  if (highway)
+  {
+    for (const auto& node : way.nodes())
+      nodeSet.emplace(node.ref());
+  }
+}
