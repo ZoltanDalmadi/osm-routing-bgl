a88b19f9a6d7cd29e42b804890a096841f44983a
Zoltan Dalmadi <dmz985@gmail.com>
2014-12-09 14:46:53 +0100

    Added getter for route length vector

--- include/RoutePlanner.h
+++ include/RoutePlanner.h
@@ -13,14 +13,21 @@ private:
   const osmium::Location start;
   const osmium::Location destination;
   std::vector<osmium::Location> route;
+  std::vector<double> distances;
 
 public:
   RoutePlanner(const GraphBuilder& gb,
                const osmium::Location& start, const osmium::Location& dest);
   const std::vector<osmium::Location>& get_route() const;
+  const std::vector<double>& get_distances() const;
 };
 
 inline const std::vector<osmium::Location>& RoutePlanner::get_route() const
 {
   return route;
+}
+
+inline const std::vector<double> &RoutePlanner::get_distances() const
+{
+  return distances;
 }
